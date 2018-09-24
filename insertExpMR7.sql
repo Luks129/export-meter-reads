b7acd7f901e9142c8485e05ec2fa41f39631b290
Luks129 <43547214+Luks129@users.noreply.github.com>
2018-09-24 16:25:09 -0300

    Create insertExpMR7.sql

---
+++ insertExpMR7.sql
@@ -0,0 +1,4 @@
+INSERT INTO EMAPP."ExpMR7"
+("SDP", "Meter", "startDate", "endDate", "channel", "extMeasCode", "channelType")
+VALUES
+('1-159U8', '1-159TU', TO_DATE('2008-11-11 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), NULL, '1-159WU', '1-159WU', 'Cumulative Consumption');
