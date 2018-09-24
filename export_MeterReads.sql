5e6e200fc4daa7cd2eae4efeb0fc2cdd61156875
Luks129 <43547214+Luks129@users.noreply.github.com>
2018-09-24 16:48:28 -0300

    Update export_MeterReads.sql

--- export_MeterReads.sql
+++ export_MeterReads.sql
@@ -1,5 +1,5 @@
-DECLARE
-  lSPD NUMBER;
+declare
+  lSPD number;
   lDeviceID number;
   lChannelID number;
   lMeasTypeID number;
@@ -10,21 +10,21 @@ DECLARE
   lEndDate DATE := TO_DATE('2018-01-12 03:00:00', 'YYYY-MM-DD HH24:MI:SS');
   
 
-  v_xml           VARCHAR2(10000);
-  v_blob          clob;
-  v_sql           VARCHAR2(4000);
-  v_xml_insert           VARCHAR2(4000);
-  a_count number;
-  a_countReads number;
-BEGIN
+  v_xml VARCHAR2(10000);
+  v_clob  clob;
+  v_sql VARCHAR2(4000);
+  v_xml_insert  VARCHAR2(4000);
+  a_count  number;
+  a_countReads  number;
+begin
   
-   v_xml               := '';
+   v_xml := '';
   
-   SELECT COUNT(*) INTO a_count FROM BTXML1;
+   SELECT COUNT(*) INTO a_count FROM ExpMR8;
    
    IF a_count>0
    THEN
-      v_sql := 'TRUNCATE TABLE BTXML1';
+      v_sql := 'TRUNCATE TABLE ExpMR8';
       EXECUTE IMMEDIATE v_sql;            
       COMMIT;
    END IF;
@@ -38,12 +38,12 @@ BEGIN
   							 null;
       END;
 
-    DBMS_LOB.CREATETEMPORARY(v_blob,true);  
+    DBMS_LOB.CREATETEMPORARY(v_clob,true);  
 				         
       
 	--Header  
     v_xml := '<MeterReadsReplyMessage xmlns="http://www.emeter.com/energyip/amiinterface" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><Header><verb>reply</verb><noun>MeterReads</noun><revision>2</revision><dateTime>2013-05-25T17:40:53</dateTime><source>SOURCE1</source></Header><payload><MeterReading><Meter><mRID>' || lDeviceID  || '</mRID><idType>METER_X_ELECTRONIC_ID</idType><pathName>SOURCE1</pathName></Meter><IntervalBlock><readingTypeId>' || lChannelID  || '</readingTypeId><IReading><endTime>' || TO_CHAR(lReadTime, 'YYYY-MM-DD HH24:MI:SS')  || '</endTime><value>' || lReadValue  || '</value><flags>0</flags></IReading></IntervalBlock>';
-	DBMS_LOB.APPEND(v_blob, v_xml);
+	DBMS_LOB.APPEND(v_clob, v_xml);
 	
 	--Select Interval Data
 	/*
@@ -63,12 +63,12 @@ BEGIN
 	
 	--Interval Data readingTypeId
     v_xml := '<IntervalBlock><readingTypeId>' || lMeasTypeID  || '</readingTypeId>';
-	DBMS_LOB.APPEND(v_blob, v_xml);
+	DBMS_LOB.APPEND(v_clob, v_xml);
 
   FOR it_data IN(
-    SELECT SDP, Meter, startDate, endDate, channel, extMeasCode
+    SELECT SDP, Meter, startDate, endDate, channel, extMeasCode, channelType
 		--INTO lChannelID, lMeasTypeID, lReadTime, lReadValue 
-		FROM TABLE_DATA TD)      
+		FROM ExpMR7)      
 		-- EXCEPTION  -- exception handlers begin
 		--					 WHEN NO_DATA_FOUND THEN  		
 		--				 null
@@ -76,11 +76,12 @@ BEGIN
 	BEGIN
     
     FOR it_mudr IN (
-      SELECT * FROM MUDR WHERE channel_ID=id_data.channel         
-    
+      SELECT LOCAL_READ_TIME RRT, CUM_READ RRVALUE
+      FROM REGISTER_READS WHERE channel_ID=id_data.channel         
+      ORDER BY LOCAL_READ_TIME DESC
 	--Loop with meter reads
-			v_xml := '<IReading><endTime>' || TO_CHAR(it_reads.IET, 'YYYY-MM-DD HH24:MI:SS')  || '</endTime><value>' || it_reads.LPVALUE  || '</value><flags>0</flags></IReading>';
-			DBMS_LOB.APPEND(v_blob, v_xml);
+			v_xml := '<IReading><endTime>' || TO_CHAR(it_reads.RRT, 'YYYY-MM-DD HH24:MI:SS')  || '</endTime><value>' || it_reads.RRVALUE  || '</value><flags>0</flags></IReading>';
+			DBMS_LOB.APPEND(v_clob, v_xml);
 		END;
 	  END LOOP;	
 	 --End Interval Data
@@ -89,11 +90,11 @@ BEGIN
 	
 	--Footer
 	v_xml := '</IntervalBlock></MeterReading></payload></MeterReadsReplyMessage>';
-	DBMS_LOB.APPEND(v_blob, v_xml);
+	DBMS_LOB.APPEND(v_clob, v_xml);
 	
-	v_xml_insert  := 'INSERT INTO BTXML1 VALUES (:1, :1 )';
-	EXECUTE IMMEDIATE v_xml_insert USING lDeviceID,v_blob;
+	v_xml_insert  := 'INSERT INTO ExpMR8 VALUES (:1, :1 )';
+	EXECUTE IMMEDIATE v_xml_insert USING lDeviceID,v_clob;
 	  
-END;
+end;
 
-COMMIT;
+commit;
